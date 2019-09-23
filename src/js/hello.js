ea9f0fca0e09e4dc24d74d08470416dab8bb504c
baena <nayeon0113@naver.com>
2019-09-23 18:23:20 +0900

    190923 인스타 레이아웃 커버

--- src/js/hello.js
+++
@@ -1,9 +0,0 @@
-const titleWrap = document.querySelector('#title');
-const msgWrap = document.querySelector('#msg');
-
-const title = () => 'Hi there!!';
-const msg = () => 'Hope you happy hacking with this package.';
-window.onload = () => {
-    titleWrap.innerHTML = title();
-    msgWrap.innerHTML = msg();
-}
